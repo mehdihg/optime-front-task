bd48882f2d1db1b6ea77deda7eb45befba21983b
mehdihg <mahdihaghanee@gmail.com>
2023-07-15 02:21:00 +0330

    add style

--- src/comments/singleComment.js
+++ src/comments/singleComment.js
@@ -1,10 +1,14 @@
-import React, {  useState } from "react";
-import {  useDispatch } from "react-redux";
-import { FaRegUserCircle } from "react-icons/fa";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+// import { FaRegUserCircle } from "react-icons/fa";
 import "../App.css";
 import { ulid } from "ulid";
 import CommentForm from "../components/CommentForm";
-import { editComment, removeComment,addComment } from "../provider/commentSlice";
+import {
+  editComment,
+  removeComment,
+  addComment,
+} from "../provider/commentSlice";
 import Button from "../components/Button";
 function SingleComment({ item }) {
   const dispatch = useDispatch();
@@ -16,10 +20,9 @@ function SingleComment({ item }) {
   };
   const handleEdit = (body, id) => {
     dispatch(editComment({ id, body }));
-    setActiveEdit(false)
+    setActiveEdit(false);
   };
-  const handleReply=(body, id)=>{
-
+  const handleReply = (body, id) => {
     const cm = {
       id: ulid(),
       body: body,
@@ -28,34 +31,44 @@ function SingleComment({ item }) {
       parentId: id,
       createdAt: Date.now(),
     };
-    dispatch(addComment(cm))
-    setActiveRes(false)
-  }
+    dispatch(addComment(cm));
+    setActiveRes(false);
+  };
   return (
     <div>
       <div className="comment-body">
         <div className="comment-user">
-          <FaRegUserCircle />
+          <div className="comment-user-avatar">
+            <span>{item.username[0]}</span>
+          </div>
           <div className="comment-username">
             <span>{item.username}</span>
             <span>{new Date(item.createdAt).toLocaleDateString()}</span>
           </div>
         </div>
-        <p>{item.body}</p>
+        <p className="comment-item-body">{item.body}</p>
         <div className="comment-body-buttons">
           {activeRes ? (
-              <Button text='بستن' isOpen={activeRes} setIsOpen={setActiveRes}/>
+            <Button text="بستن" isOpen={activeRes} setIsOpen={setActiveRes} />
           ) : (
             item.parentId === null && (
-              <Button text='پاسخ به' isOpen={activeRes} setIsOpen={setActiveRes}/>
+              <Button
+                text="پاسخ"
+                isOpen={activeRes}
+                setIsOpen={setActiveRes}
+              />
             )
           )}
 
           <button onClick={() => handleRemove(item.id)}>حذف</button>
           {activeEdit & item.id ? (
-              <Button text='بستن' isOpen={activeEdit} setIsOpen={setActiveEdit}/>
+            <Button text="بستن" isOpen={activeEdit} setIsOpen={setActiveEdit} />
           ) : (
-            <Button text='ویرایش' isOpen={activeEdit} setIsOpen={setActiveEdit}/>
+            <Button
+              text="ویرایش"
+              isOpen={activeEdit}
+              setIsOpen={setActiveEdit}
+            />
           )}
         </div>
         {activeRes && <CommentForm submitComment={handleReply} id={item.id} />}
